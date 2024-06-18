79e43b423f217d477b87b8fbaaa133088174ab41
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-06-17 21:20:20 -0300

    feat: Basic Carousel Markup

--- src/components/Products/ProductsGrid.tsx
+++ src/components/Products/ProductsGrid.tsx
@@ -1,5 +1,5 @@
 import { products } from "../../mocks/data";
-
+import ImagesCarousel from "./ImagesCarousel";
 const ProductsGrid = () => {
   const cl = console.log;
   cl(products);
@@ -7,9 +7,14 @@ const ProductsGrid = () => {
     <div className="products-grid">
       {products.map((product, index) => (
         <div className="product-card" key={index}>
-          <img src={product.img} alt={product.name} className="product-image h-10" />
+          <ImagesCarousel images={product.img} />
+          {/* <img
+            src={product.img}
+            alt={product.name}
+            className="product-image h-10"
+          /> */}
           <h2 className="product-name">{product.name}</h2>
-          <p className="product-price">${product.price}</p>
+          <p className="product-price">${product.price} COP</p>
           <p className="product-ratings">Ratings: {product.ratings}</p>
           <p className="product-description">{product.description}</p>
           <p className="product-advice">{product.advice}</p>
--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,7 +1,13 @@
 export const products = [
   {
     name: "Bombones de Chocolate - Caja x 48",
-    img: "./public/images/img-23.webp",
+    img: [
+      "./public/images/img-23.webp",
+      "./public/images/img-23.webp",
+      "./public/images/img-23.webp",
+      "./public/images/img-23.webp",
+      "./public/images/img-23.webp",
+    ],
     price: 176000,
     ratings: 5.0,
     description: "Chocolate natural",
@@ -10,7 +16,7 @@ export const products = [
   },
   {
     name: "Bombones de Chocolate - Caja x 24",
-    img: "./public/images/img-23.webp",
+    img: ["./public/images/img-23.webp"],
     price: 91800,
     ratings: 5.0,
     description: "Chocolate natural",
@@ -19,7 +25,7 @@ export const products = [
   },
   {
     name: "Bombones de Chocolate - Caja x 12",
-    img: "./public/images/img-23.webp",
+    img: ["./public/images/img-23.webp"],
     price: 44500,
     ratings: 5.0,
     description: "Chocolate natural",
@@ -28,7 +34,7 @@ export const products = [
   },
   {
     name: "Chocolatina Frutas Deshidratadas",
-    img: "./public/images/img-23.webp",
+    img: ["./public/images/img-23.webp"],
     price: 24000,
     ratings: 5.0,
     description: "Chocolate natural",
@@ -37,7 +43,7 @@ export const products = [
   },
   {
     name: "Bombas de Chocolate - Caja x 2",
-    img: "./public/images/img-23.webp",
+    img: ["./public/images/img-23.webp"],
     price: 28000,
     ratings: 5.0,
     description: "Chocolate natural",
@@ -46,7 +52,7 @@ export const products = [
   },
   {
     name: "Bombones de Chocolate - Caja x 6",
-    img: "./public/images/img-23.webp",
+    img: ["./public/images/img-23.webp"],
     price: 22500,
     ratings: 5.0,
     description: "Chocolate natural",

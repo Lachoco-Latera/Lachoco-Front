9bbe5347dcc26729a8a139d44dc977bf75df47fa
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-07-01 03:17:36 -0300

    fix: Categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -19,6 +19,7 @@ import {
   MdOutlineCookie,
 } from "react-icons/md";
 import { FaTreeCity, FaIceCream } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 const Categories = ({ categories }: any) => {
   const iconMap: Record<string, IconType> = {
@@ -41,6 +42,7 @@ const Categories = ({ categories }: any) => {
 
   const [searchParams, setSearchParams] = useSearchParams();
   const selectedCategory = searchParams.get("category") || "default";
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   const handleClickCategory = (category: string) => {
     const currentCategory = searchParams.get("category");
@@ -50,10 +52,22 @@ const Categories = ({ categories }: any) => {
       setSearchParams({ category });
     }
   };
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 sticky top-14 bg-white z-[5] drop-shadow">
-      <div className="pt-4 flex flex-row text-center items-center justify-between overflow-x-auto">
+    <div
+      className={`max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 sticky top-[66px] bg-white z-[5] transition-all ease ${
+        hasScrolled ? "drop-shadow" : ""
+      }`}
+    >
+      <div className=" flex flex-row text-center items-center justify-between overflow-x-auto">
         {categories.map((item: any) => (
           <CategoryBox
             key={item.id}

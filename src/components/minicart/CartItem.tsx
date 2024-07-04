e048ef43249c2734aef855eff444d6296ae0c4ab
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-07-04 08:52:07 -0300

    feat: Functional steps CartItem

--- src/components/minicart/CartItem.tsx
+++ src/components/minicart/CartItem.tsx
@@ -1,10 +1,11 @@
-import { FaExclamation, FaTrashAlt } from "react-icons/fa";
-
+import { FaCheck, FaExclamation, FaTrashAlt } from "react-icons/fa";
 import { Product } from "../../types";
 import { useCartStore } from "../../stores/useCartStore";
 import { toast } from "sonner";
 import { IoIosAdd } from "react-icons/io";
 import { LuMinus } from "react-icons/lu";
+import { useEffect, useState } from "react";
+
 interface Props {
   product: Product;
 }
@@ -14,17 +15,44 @@ export default function CartItem({ product }: Props) {
   const addToCart = useCartStore((state) => state.addToCart);
   const delFromCart = useCartStore((state) => state.subtractFromCart);
   const cart = useCartStore((state) => state.cart);
-  // const { confirmedFlavors } = useCartStore();
+  const confirmedFlavors = useCartStore((state) => state.confirmedFlavors); // Obtener confirmedFlavors del store
+  const [confirmedFlavorsCount, setConfirmedFlavorsCount] = useState<number>(0);
+  const [, setActualTotalFlavors] = useState<number>(0);
+
+  const selectedProductId = product.id;
+
+  useEffect(() => {
+    // Calcular la cantidad de veces que selectedProductId aparece en confirmedFlavors
+    let count = 0;
+    if (confirmedFlavors[selectedProductId]) {
+      count = confirmedFlavors[selectedProductId].length;
+    }
+    setConfirmedFlavorsCount(count);
+
+    // Sumar todos los valores de los arrays en confirmedFlavors
+    let totalFlavors = 0;
+    for (const productId in confirmedFlavors) {
+      totalFlavors += confirmedFlavors[productId].length;
+    }
+    setActualTotalFlavors(totalFlavors);
+  }, [confirmedFlavors, selectedProductId]);
   //@ts-ignore
   const productMaxFlavor = product.quantity * product.presentacion;
-  const selectedProductId = product.id;
-  console.log(productMaxFlavor, selectedProductId);
+  // console.log(
+  //   "Confirmación individual del cliente",
+  //   confirmedFlavorsCount,
+  //   "Sumatoria:",
+  //   actualTotalFlavors
+  // );
+  // console.log(
+  //   "Confirmación individual del cliente",
+  //   confirmedFlavorsCount,
+  //   "Máximo:",
+  //   productMaxFlavor
+  // );
   const handleSubtractFromCart = (product: Product) => {
     const cartItem = cart.find((item) => item.id === product.id);
-    if (
-      //@ts-ignore
-      cartItem?.quantity === 1
-    ) {
+    if (cartItem?.quantity === 1) {
       removeFromCart(product);
       toast.error(`Producto ${product.name} fue eliminado 🗑`);
     } else {
@@ -32,6 +60,7 @@ export default function CartItem({ product }: Props) {
       toast.info(`Producto ${product.name} retirado`);
     }
   };
+
   return (
     <li className="flex justify-between items-center gap-4 mb-2 shadow-md p-4">
       <div
@@ -51,14 +80,13 @@ export default function CartItem({ product }: Props) {
             <span className="font-light"> x {product.presentacion}</span>
           </span>
           <span className="text-gray-600 font-bold">${product.price}</span>
-
-          <span>
+          <span className="flex flex-row gap-2 items-center">
             Cajas:
-            {
-              //@ts-ignore
-              product.quantity
-            }
-            {product.quantity ? <FaExclamation /> : null}
+            {confirmedFlavorsCount !== productMaxFlavor ? (
+              <FaExclamation className=" text-amber-500" size={16} />
+            ) : (
+              <FaCheck className=" text-green-500" />
+            )}
           </span>
         </div>
       </div>

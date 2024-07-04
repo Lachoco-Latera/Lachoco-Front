691850853f2430476b12d6e0205568fd8937d371
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-07-04 01:20:53 -0300

    fix: Testing selectFlavors States

--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -8,18 +7,21 @@ interface State {
   cart: Product[];
   totalItems: number;
   totalPrice: number;
+  flavorsSelected: boolean;
 }
 
 interface Actions {
-  addToCart: (Item: Product) => void;
-  removeFromCart: (Item: Product) => void;
-  substractFromCart: (Item: Product) => void;
+  addToCart: (product: Product) => void;
+  removeFromCart: (product: Product) => void;
+  subtractFromCart: (product: Product) => void;
+  selectFlavors: (product: Product, flavorsSelected: boolean) => void;
 }
 
 const INITIAL_STATE: State = {
   cart: [],
   totalItems: 0,
   totalPrice: 0,
+  flavorsSelected: false,
 };
 
 export const useCartStore = create(
@@ -28,6 +30,7 @@ export const useCartStore = create(
       cart: INITIAL_STATE.cart,
       totalItems: INITIAL_STATE.totalItems,
       totalPrice: INITIAL_STATE.totalPrice,
+      flavorsSelected: INITIAL_STATE.flavorsSelected,
       addToCart: (product: Product) => {
         const cart = get().cart;
         const cartItem = cart.find((item) => item.id === product.id);
@@ -38,19 +41,44 @@ export const useCartStore = create(
               ? { ...item, quantity: (item.quantity as number) + 1 }
               : item
           );
-          set((state) => ({
-            cart: updatedCart,
-            totalItems: state.totalItems + 1,
-            totalPrice: state.totalPrice + product.price,
-          }));
+          set((state) => {
+            const updatedState = {
+              ...state,
+              cart: updatedCart,
+              totalItems: state.totalItems + 1,
+              totalPrice: state.totalPrice + parseFloat(product.price), // Convertir a number
+            };
+            if (product.category && product.category.name === "bombones") {
+              const bombonesCount = updatedState.cart.filter(
+                (item) => item.category.name === "bombones"
+              ).length;
+              console.log(
+                `Existe ${bombonesCount} productos que son bombones, por favor elegir sabores`
+              );
+            }
+            return updatedState;
+          });
         } else {
           const updatedCart = [...cart, { ...product, quantity: 1 }];
 
-          set((state) => ({
-            cart: updatedCart,
-            totalItems: state.totalItems + 1,
-            totalPrice: state.totalPrice + product.price,
-          }));
+          set((state) => {
+            const updatedState = {
+              ...state,
+              cart: updatedCart,
+              totalItems: state.totalItems + 1,
+              totalPrice: state.totalPrice + parseFloat(product.price), // Convertir a number
+              flavorsSelected: false, // Reiniciar selección de sabores para nuevos productos
+            };
+            if (product.category && product.category.name === "bombones") {
+              const bombonesCount = updatedState.cart.filter(
+                (item) => item.category.name === "bombones"
+              ).length;
+              console.log(
+                `Agregaste ${bombonesCount} producto que son bombones, por favor elegir sabores`
+              );
+            }
+            return updatedState;
+          });
         }
       },
       removeFromCart: (product: Product) => {
@@ -60,14 +88,16 @@ export const useCartStore = create(
         if (cartItem) {
           const updatedCart = cart.filter((item) => item.id !== product.id);
           set((state) => ({
+            ...state,
             cart: updatedCart,
             totalItems: state.totalItems - (cartItem.quantity as number),
             totalPrice:
-              state.totalPrice - cartItem.price * (cartItem.quantity as number),
+              state.totalPrice -
+              parseFloat(cartItem.price) * (cartItem.quantity as number), // Convertir a number
           }));
         }
       },
-      substractFromCart: (product: Product) => {
+      subtractFromCart: (product: Product) => {
         const cart = get().cart;
         const cartItem = cart.find((item) => item.id === product.id);
 
@@ -75,9 +105,10 @@ export const useCartStore = create(
           if (cartItem.quantity === 1) {
             const updatedCart = cart.filter((item) => item.id !== product.id);
             set((state) => ({
+              ...state,
               cart: updatedCart,
               totalItems: state.totalItems - 1,
-              totalPrice: state.totalPrice - product.price,
+              totalPrice: state.totalPrice - parseFloat(product.price), // Convertir a number
             }));
           } else {
             const updatedCart = cart.map((item) =>
@@ -86,11 +117,23 @@ export const useCartStore = create(
                 : item
             );
             set((state) => ({
+              ...state,
               cart: updatedCart,
               totalItems: state.totalItems - 1,
-              totalPrice: state.totalPrice - product.price,
+              totalPrice: state.totalPrice - parseFloat(product.price), // Convertir a number
             }));
           }
+        }
+      },
+      selectFlavors: (product: Product, flavorsSelected: boolean) => {
+        if (product.category && product.category.name === "bombones") {
+          set((state) => ({
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === product.id ? { ...item, flavorsSelected } : item
+            ),
+            flavorsSelected,
+          }));
         }
       },
     }),

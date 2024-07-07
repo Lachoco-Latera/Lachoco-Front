1db22479c8ebef9e8011cf0e35b07eeb770145c8
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-07-06 21:47:51 -0300

    feat: Integration Shipment / Style

--- src/components/Ship/Ship.tsx
+++ src/components/Ship/Ship.tsx
@@ -104,176 +104,158 @@ const Ship = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center p-12 shadow-xl">
-      <div className="flex flex-col justify-center items-center p-12 shadow-xl rounded-xl overflow-hidden">
+    <div className="">
+      <div className="flex flex-col justify-center items-center p-12 shadow-xl rounded-xl ">
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col items-center mb-4 border-b-4 border-green-400 pb-2"
+          className="flex flex-col items-center mb-4 border-b-4 border-green-400 pb-2 sm:flex-row sm:items-center sm:justify-center"
         >
-          <label htmlFor="countryCode">Country Code:</label>
+          <label htmlFor="countryCode" className="mr-2">
+            Country Code:
+          </label>
           <input
             type="text"
             id="countryCode"
             value={countryCode}
             onChange={(e) => setCountryCode(e.target.value)}
-            className="p-2 border mb-2"
+            className="p-2 border mb-2 sm:mb-0 sm:mr-2"
           />
           <button type="submit" className="p-2 bg-blue-500 text-white rounded">
             Fetch Data
           </button>
         </form>
-        <div className="w-full overflow-x-auto overflow-y-auto mb-4 max-h-[500px] border-b-4 border-yellow-400 pb-2">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Service ID
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Carrier Name
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Description
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Delivery Estimate
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Active
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {info ? (
-                info.data.map((carrier: any, index: number) => (
-                  <tr key={`carrier-${carrier.service_id}`}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {carrier.service_id}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {carrier.carrier_name}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {carrier.description}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {carrier.delivery_estimate}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {carrier.active === 1 ? "Yes" : "No"}
+        <div className="md:w-full w-3/4 overflow-x-auto overflow-y-auto mb-4 max-h-[500px] border-b-4 border-yellow-400 pb-2">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Service ID
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Carrier Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Description
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Delivery Estimate
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Active
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {info ? (
+                  info.data.map((carrier: any) => (
+                    <tr key={`carrier-${carrier.service_id}`}>
+                      <td className="px-6 py-4 ">{carrier.service_id}</td>
+                      <td className="px-6 py-4 ">{carrier.carrier_name}</td>
+                      <td className="px-6 py-4 ">{carrier.description}</td>
+                      <td className="px-6 py-4 ">
+                        {carrier.delivery_estimate}
+                      </td>
+                      <td className="px-6 py-4 ">
+                        {carrier.active === 1 ? "Yes" : "No"}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr key="no-data">
+                    <td colSpan={5} className="px-6 py-4  text-center">
+                      No data available
                     </td>
                   </tr>
-                ))
-              ) : (
-                <tr key="no-data">
-                  <td
-                    colSpan={5}
-                    className="px-6 py-4 whitespace-nowrap text-center"
-                  >
-                    No data available
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="w-full overflow-x-auto overflow-y-auto mb-4 max-h-[500px] border-b-4 border-red-400 pb-2">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Country Code
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Country Name
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Phone Code
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {countries.length > 0 ? (
-                countries.map((country: any, index: number) => (
-                  <tr key={`country-${country.code}-${index}`}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {country.code}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {country.name}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {country.phone_code}
+        <div className="md:w-full w-3/4 overflow-x-auto overflow-y-auto mb-4 max-h-[500px] border-b-4 border-red-400 pb-2">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Country Code
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Country Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Phone Code
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200 ">
+                {countries.length > 0 ? (
+                  countries.map((country: any, index: number) => (
+                    <tr
+                      key={`country-${country.code}-${index}`}
+                      className="max-w-12"
+                    >
+                      <td className="px-6 py-4 max-w-12">{country.code}</td>
+                      <td className="px-6 py-4 max-w-12">{country.name}</td>
+                      <td className="px-6 py-4 max-w-12">
+                        {country.phone_code}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr key="no-country-data">
+                    <td colSpan={3} className="px-6 py-4  text-center">
+                      No country data available
                     </td>
                   </tr>
-                ))
-              ) : (
-                <tr key="no-country-data">
-                  <td
-                    colSpan={3}
-                    className="px-6 py-4 whitespace-nowrap text-center"
-                  >
-                    No country data available
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="w-full overflow-x-auto overflow-y-auto  max-h-[500px] border-b-4 border-blue-400 pb-2">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  State Name
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Code (2 digits)
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Code (3 digits)
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Shopify Code
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Country Code
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {states.length > 0 ? (
-                states.map((state: any, index: number) => (
-                  <tr key={`state-${state.code_2_digits}-${index}`}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {state.name}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {state.code_2_digits}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {state.code_3_digits}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {state.code_shopify}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {state.country_code}
+        <div className="md:w-full w-3/4 overflow-x-auto overflow-y-auto  max-h-[500px] border-b-4 border-blue-400 pb-2">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    State Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Code (2 digits)
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Code (3 digits)
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Shopify Code
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Country Code
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {states.length > 0 ? (
+                  states.map((state: any, index: number) => (
+                    <tr key={`state-${state.code_2_digits}-${index}`}>
+                      <td className="px-6 py-4 ">{state.name}</td>
+                      <td className="px-6 py-4 ">{state.code_2_digits}</td>
+                      <td className="px-6 py-4 ">{state.code_3_digits}</td>
+                      <td className="px-6 py-4 ">{state.code_shopify}</td>
+                      <td className="px-6 py-4 ">{state.country_code}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr key="no-state-data">
+                    <td colSpan={5} className="px-6 py-4  text-center">
+                      No state data available
                     </td>
                   </tr>
-                ))
-              ) : (
-                <tr key="no-state-data">
-                  <td
-                    colSpan={5}
-                    className="px-6 py-4 whitespace-nowrap text-center"
-                  >
-                    No state data available
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
         {errors && (
           <div className="text-red-500">

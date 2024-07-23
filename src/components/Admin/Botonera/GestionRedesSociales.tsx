3ca97ff6e25e871b36a29a60be0046cb0b061139
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-07-23 06:03:02 -0300

    fix: Redes

--- src/components/Admin/Botonera/GestionRedesSociales.tsx
+++ src/components/Admin/Botonera/GestionRedesSociales.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   deleteRed,
   getRedes,
@@ -15,12 +14,8 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
     url: "",
     img: "",
   });
-  const [formEditState, setFormEditState] = useState<IRedes>({
-    url: "",
-    img: "",
-  });
   const [data, setData] = useState<IRedes[]>([]);
-  signal;
+  const [selectedRed, setSelectedRed] = useState<IRedes | any>(null);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
@@ -29,10 +24,6 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
     }
   }, [signal]);
 
-  const closeModal = () => {
-    setModalOpen(false);
-    onCloseModal(); // Llama a la función para cerrar el modal y actualizar signal en Admin
-  };
   useEffect(() => {
     const dataGet = async () => {
       try {
@@ -43,44 +34,29 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
       }
     };
     dataGet();
-  }, [formState]);
+  }, []);
 
-  const handleEdit = (event: React.MouseEvent) => {
-    event.preventDefault();
-   
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const alertId = toast("¿Deseas editar la orden seleccionada?", {
-      duration: 5000,
-      action: {
-        label: "Aceptar",
-        onClick: async () => {
-          toast.dismiss(alertId);
-          try {
-            // Aquí se debería agregar la lógica para eliminar la orden usando su ID
-           //
-           setModalOpen(true);
-           setEditState(!editState)
-          } catch (error) {
-            console.log(error);
-          }
-        },
-      },
-    });
+  useEffect(() => {
+    if (selectedRed) {
+      setFormState(selectedRed);
+    }
+  }, [selectedRed]);
+
+  const closeModal = () => {
+    setModalOpen(false);
+    onCloseModal();
+  };
+
+  const handleEdit = (red: IRedes) => {
+    setSelectedRed(red);
+    setEditState(true);
+    setModalOpen(true);
   };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
-      [name]: value,
-    });
-  };
-  const handleOnChangeEdit = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-
-    setFormEditState({
-      ...formEditState,
       [name]: value,
     });
   };
@@ -88,17 +64,18 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
   const handleDelete = async (id: string | undefined) => {
     try {
       const alertId = toast("¿Deseas eliminar la red social seleccionada?", {
-        duration: Infinity, // Mantiene la alerta abierta hasta que se haga clic en un botón
+        duration: Infinity,
         action: {
           label: "Aceptar",
           onClick: async () => {
             toast.dismiss(alertId);
             try {
-              const cardDelete = await deleteRed(id);
-              console.log(cardDelete);
-              toast.success ('La orden se elimino correctamente',{
+              await deleteRed(id);
+              toast.success("La red social se eliminó correctamente", {
                 duration: 5000,
-              } )
+              });
+              const data = await getRedes();
+              setData(data);
             } catch (error) {
               console.log(error);
             }
@@ -114,17 +91,19 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
     event.preventDefault();
     try {
       const alertId = toast("¿Deseas agregar esta red social?", {
-        duration: Infinity, // Mantiene la alerta abierta hasta que se haga clic en un botón
+        duration: Infinity,
         action: {
           label: "Aceptar",
           onClick: async () => {
             toast.dismiss(alertId);
             try {
-              const postForm = await postRedes(formState);
-              postForm;
-              toast.success ('La orden se agrego correctamente',{
+              await postRedes(formState);
+              toast.success("La red social se agregó correctamente", {
                 duration: 5000,
-              } )
+              });
+              const data = await getRedes();
+              setData(data);
+              closeModal();
             } catch (error) {
               console.log(error);
             }
@@ -135,19 +114,28 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
       console.log(error);
     }
   };
-
   const handleOnSubmitEdit = async (event: React.FormEvent) => {
     event.preventDefault();
+    if (!selectedRed) return;
+
     try {
       const alertId = toast("¿Deseas editar esta red social?", {
-        duration: Infinity, // Mantiene la alerta abierta hasta que se haga clic en un botón
+        duration: Infinity,
         action: {
           label: "Aceptar",
           onClick: async () => {
             toast.dismiss(alertId);
             try {
-              const putForm = await putRedes(formEditState);
-              putForm;
+              console.log(formState);
+
+              // Asegúrate de pasar el id correcto aquí
+              await putRedes(selectedRed.id, formState);
+              toast.success("La red social se actualizó correctamente", {
+                duration: 5000,
+              });
+              const data = await getRedes();
+              setData(data);
+              closeModal();
             } catch (error) {
               console.log(error);
             }
@@ -168,96 +156,68 @@ export const GestionRedesSociales = ({ signal, onCloseModal }: any) => {
             onClick={closeModal}
           >
             <div
-              className="bg-white p-6 rounded-lg"
+              className="bg-white p-6 rounded-lg max-w-full md:max-w-[500px] mx-4 md:mx-auto"
               onClick={(e) => e.stopPropagation()}
             >
               <form
-                action=""
-                onSubmit={handleOnSubmit}
-                className="w-[500px] h-[300px] flex flex-col justify-evenly items-center  "
+                onSubmit={editState ? handleOnSubmitEdit : handleOnSubmit}
+                className="w-full h-auto flex flex-col justify-evenly items-center p-4 rounded-lg"
               >
-                <h2>Agregar nueva red social</h2>
+                <h2>
+                  {editState ? "Editar red social" : "Agregar nueva red social"}
+                </h2>
                 <input
                   type="text"
-                  placeholder="url"
+                  placeholder="URL"
                   name="url"
                   value={formState.url}
                   onChange={handleOnChange}
-                  className="w-2/3 p-2 rounded-md border-2 border-gray-300"
+                  className="p-2 border rounded-md mb-2 w-full"
                 />
                 <input
                   type="text"
-                  placeholder="img"
+                  placeholder="Imagen"
                   name="img"
                   value={formState.img}
                   onChange={handleOnChange}
-                  className="w-2/3 p-2 rounded-md border-2 border-gray-300"
+                  className="p-2 border rounded-md mb-2 w-full"
                 />
-                <button className="w-2/3 h-[40px] xl:text-xl text-white p-1 block rounded-lg font-semibold duration-400 bg-green-500 hover:bg-green-900 hover:text-green-500 m-3 capitalize">
-                  Agregar
+                <button className="w-full h-[40px] text-white p-1 block rounded-lg font-semibold bg-green-500 hover:bg-green-600 m-3 capitalize transition-all duration-300 ease-in-out transform hover:scale-105">
+                  {editState ? "Guardar Cambios" : "Agregar"}
                 </button>
               </form>
-            </div>{" "}
+            </div>
           </div>
         )}
-        {editState === true ? (
-          <form
-            action=""
-            className="w-[500px] h-[300px] flex flex-col justify-evenly items-center bg-violet-300"
-            onSubmit={handleOnSubmitEdit}
-          >
-            <h2>Editar red social</h2>
-            <input
-              type="text"
-              placeholder="url"
-              name="url"
-              className="w-2/3 p-2 rounded-md border-2 border-gray-300"
-              value={formEditState.url}
-              onChange={handleOnChangeEdit}
-            />
-            <input
-              type="text"
-              placeholder="img"
-              name="img"
-              className="w-2/3 p-2 rounded-md border-2 border-gray-300"
-              value={formEditState.img}
-              onChange={handleOnChangeEdit}
-            />
-            <button className="w-2/3 h-[40px] xl:text-xl text-white p-1 block rounded-lg font-semibold duration-400 bg-green-500 hover:bg-green-900 hover:text-green-500 m-3 capitalize">
-              Guardar Cambios
-            </button>
-          </form>
-        ) : null}
         <div className="w-full flex justify-evenly items-center flex-wrap px-4 py-8 gap-4">
           {data.length > 0
             ? data.map((elem) => (
                 <div
-                  className="w-[300px] min-h-[350px] flex flex-col
-          justify-evenly items-center bg-white p-4 
-          rounded-xl shadow-xl hover:shadow-xl
-          transition-all ease  
-          hover:scale-105"
+                  className="w-full sm:w-[300px] min-h-[350px] flex flex-col justify-evenly items-center bg-white p-4 rounded-xl shadow-xl hover:shadow-xl transition-all ease hover:scale-105"
                   key={elem.id}
                 >
                   <p className="flex font-bold text-amber-300 text-center">
                     {elem.url}
                   </p>
                   <p className="font-bold text-amber-300 text-center">
-                    <img src={elem.img} alt="" />
+                    <img
+                      src={elem.img}
+                      alt=""
+                      className="w-36 h-36 object-cover"
+                    />
                   </p>
-
                   <div className="w-full flex justify-center items-center">
                     <button
-                      className="w-1/3 h-[40px] xl:text-xl text-white p-1 block rounded-2xl font-semibold duration-400 bg-yellow-600 hover:bg-yellow-900 hover:text-yellow-500 m-3 capitalize hover:scale-105 transition-all ease"
-                      onClick={handleEdit}
+                      className="w-1/2 h-[40px] text-white p-1 block rounded-2xl font-semibold bg-yellow-600 hover:bg-yellow-700 m-3 capitalize transition-all duration-300 ease-in-out transform hover:scale-105"
+                      onClick={() => handleEdit(elem)}
                     >
-                      editar
+                      Editar
                     </button>
                     <button
-                      className="w-1/3 h-[40px] xl:text-xl text-white p-1 block rounded-2xl font-semibold duration-400 bg-red-500 hover:bg-red-900 hover:text-red-500 m-3 capitalize hover:scale-105 transition-all ease"
+                      className="w-1/2 h-[40px] text-white p-1 block rounded-2xl font-semibold bg-red-500 hover:bg-red-600 m-3 capitalize transition-all duration-300 ease-in-out transform hover:scale-105"
                       onClick={() => handleDelete(elem.id)}
                     >
-                      eliminar
+                      Eliminar
                     </button>
                   </div>
                 </div>
--- src/helpers/service.ts
+++ src/helpers/service.ts
@@ -1,72 +1,77 @@
-import axios from "axios"
-import { IFlavor, IRedes } from "./type"
+import axios from "axios";
+import { IFlavor, IRedes } from "./type";
 
 // ------------------------------------- REDES
 export const getRedes = async () => {
-    try {
-        const res = await axios.get('https://lachocoback.vercel.app/redes')
-        // console.log(res.data, '<<<<<------ service get')
-        return res.data
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const res = await axios.get("https://lachocoback.vercel.app/redes");
+    // console.log(res.data, '<<<<<------ service get')
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const postRedes = async (data: IRedes) => {
-    try {
-        const res = await axios.post('https://lachocoback.vercel.app/redes', data)
-        console.log(res.data, '<<<<<<<<<<<<<<------ service post')
-        return res.data
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const res = await axios.post("https://lachocoback.vercel.app/redes", data);
+    console.log(res.data, "<<<<<<<<<<<<<<------ service post");
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const putRedes = async (data: IRedes) => {
-    try {
-        // const res = await axios.put('', data)
-        // console.log(res.data, '<<<<<<<<<<<<<<<<<<<<<<<------------ service put redes')
-        // return res.data
-        console.log(data)
-        return( 'Data recibida: CORRECTAMENTE')
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const putRedes = async (id: string, data: IRedes) => {
+  try {
+    const res = await axios.put(`http://localhost:3000/redes/${id}`, data);
+    console.log(
+      res.data,
+      "<<<<<<<<<<<<<<<<<<<<<<<------------ service put redes"
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 export const deleteRed = async (id: string | undefined) => {
-    try {
-        // const res = await axios.delete(`${id}`)
-        // console.log(res.data, '<<<<<<<<<<<<<<<<<<<<<<<------------ service delete redes')
-        // return res.data
-        console.log(id)
-        return( 'Elemento eliminado correctamente')
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const res = await axios.delete(`${id}`);
+    console.log(
+      res.data,
+      "<<<<<<<<<<<<<<<<<<<<<<<------------ service delete redes"
+    );
+    return res.data;
+    console.log(id);
+    return "Elemento eliminado correctamente";
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// ----------------------------------------------- F L A V O R S 
+// ----------------------------------------------- F L A V O R S
 export const postFlavors = async (data: IFlavor) => {
-    try {
-        // const res = await axios.post('', data)
-        // console.log(res.data)
-        // return res.data
-        console.log(data)
-        return('Datos cargados exitosamente')
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    // const res = await axios.post('', data)
+    // console.log(res.data)
+    // return res.data
+    console.log(data);
+    return "Datos cargados exitosamente";
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deleteFlavor = async (id: string | undefined) => {
-    try {
-        // const res = await axios.delete(`${id}`)
-        // console.log(res.data)
-        // return res.data
-        console.log(id)
-        return('Sabor eliminado correctamente')
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    // const res = await axios.delete(`${id}`)
+    // console.log(res.data)
+    // return res.data
+    console.log(id);
+    return "Sabor eliminado correctamente";
+  } catch (error) {
+    console.log(error);
+  }
+};

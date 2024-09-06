befed1534ff57065e4b468e3a492df2f2d4d063a
Mateo <brandonahumada804@gmail.com>
2024-09-06 14:51:01 -0300

    Modified and new icons added

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -3,41 +3,25 @@ import { useSearchParams } from "react-router-dom";
 import CategoryBox from "../CategoryBox";
 
 // Definición de los iconos que esperas utilizar
-import { TbBeach, TbMountain, TbMilkshake } from "react-icons/tb";
-import {
-  GiBoatFishing,
-  GiField,
-  GiForestCamp,
-  GiDonut,
-  GiCupcake,
-  GiPieSlice,
-} from "react-icons/gi";
-import {
-  MdOutlineVilla,
-  MdSnowmobile,
-  MdOutlineWbSunny,
-  MdOutlineCookie,
-} from "react-icons/md";
-import { FaTreeCity, FaIceCream } from "react-icons/fa6";
+
 import { useEffect, useState } from "react";
+import { BiFoodMenu } from "react-icons/bi";
+import { PiDiamondsFourFill, PiFarmThin, PiGiftLight } from "react-icons/pi";
+import { GiChocolateBar } from "react-icons/gi";
+import { CiCoffeeCup } from "react-icons/ci";
+import { SiCoffeescript } from "react-icons/si";
+import { MdOutlineFastfood } from "react-icons/md";
 
 const Categories = ({ categories }: any) => {
   const iconMap: Record<string, IconType> = {
-    TbBeach,
-    TbMountain,
-    TbMilkshake,
-    GiBoatFishing,
-    GiField,
-    GiForestCamp,
-    GiDonut,
-    GiCupcake,
-    GiPieSlice,
-    MdOutlineVilla,
-    MdSnowmobile,
-    MdOutlineWbSunny,
-    MdOutlineCookie,
-    FaTreeCity,
-    FaIceCream,
+    PiDiamondsFourFill, 
+    PiFarmThin, 
+    PiGiftLight,
+    BiFoodMenu,
+    GiChocolateBar,
+    CiCoffeeCup,
+    MdOutlineFastfood,
+    SiCoffeescript
   };
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -70,11 +54,11 @@ const Categories = ({ categories }: any) => {
       <div className=" flex flex-row text-center items-center justify-between overflow-x-auto">
         {categories.map((item: any) => (
           <CategoryBox
-            key={item.id}
-            label={item.name}
-            icon={iconMap[item.icon]} // Utiliza iconMap para obtener el icono correcto
-            selected={selectedCategory === item.name} // Ajusta según sea necesario
-            onSelect={() => handleClickCategory(item.name)}
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.icon]} // Utiliza iconMap para obtener el icono correcto
+          selected={selectedCategory === item.name} // Ajusta según sea necesario
+          onSelect={() => handleClickCategory(item.name)}
           />
         ))}
       </div>

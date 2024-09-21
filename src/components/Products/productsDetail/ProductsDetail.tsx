db0b733ddff9af6dd764a5f08fd93a9d2a0acb33
jram828 <128335805+jram828@users.noreply.github.com>
2024-09-21 15:46:09 -0500

    fix(Flavors modal): :bug: Fixing bombones flavors

--- src/components/Products/productsDetail/ProductsDetail.tsx
+++ src/components/Products/productsDetail/ProductsDetail.tsx
@@ -40,7 +40,7 @@ const ProductsDetail: React.FC<ProductsDetailProps> = ({
   };
 
   const openFlavorModal = () => {
-    info?.category?.name === "bombones" || info?.category?.name === "chocolates de especialidad"
+    info?.category?.name === "bombones"
       ? setShowFlavorModal(true)
       : setShowFlavorModal(false);
   };
@@ -189,7 +189,7 @@ const ProductsDetail: React.FC<ProductsDetailProps> = ({
             confirmedFlavors={actualSelectionLength}
             product={info}
             openModal={
-              productInfo?.flavors ? openFlavorModal : null
+              productInfo?.category.name==="bombones" ? openFlavorModal : null
             }
             openCartModal={onCheckIconClick}
           />
